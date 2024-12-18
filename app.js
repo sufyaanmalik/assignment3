df133c80eed2d51cfa00da5a5290dc63ab739613
sufyaanmalik <sufyaanmaalik@gmail.com>
2024-12-18 09:26:21 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// CHapter # 3
+
+// Question # 1
+
+var age = 24;
+alert("I am " + age + " Years Old");
+
+
+// Question # 2
+
+let visitors = 20;
+alert("You have visited " + visitors + " times on our website");
+
+
+// Question # 3
+
+let birthyear = 2000;
+document.write("My Birth year is " + birthyear + "<br>");
+
+// Question # 4
+
+let visitorname = "John Doe";
+let product_title = "Jackets";
+let quantity = 10;
+
+document.write(visitorname + " order " + quantity + " " + product_title + " " + "On Online Store");
